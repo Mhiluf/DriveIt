eb44fb7c1832c606ed71363a686709058cf921ae
Mhiluf <79202637+Mhiluf@users.noreply.github.com>
2021-08-07 02:35:35 +0100

    Full Assignment

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,38 @@
+"""tango_with_django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from re import search
+from django.contrib import admin
+from django.urls import path
+from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('rango/about/',views.about, name='about'),
+    path('category/<slug:category_name_slug>/',views.show_category, name='show_category'),
+    path('rango/add_category/', views.add_category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/',views.add_page, name='add_page'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
+    path('restricted/', views.restricted, name='restricted'),
+    path('logout/', views.user_logout, name='logout'),
+    path('search/', views.search, name='search'),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
